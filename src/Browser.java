6e7740a39e8b3dc247fe87cb2bb62d48dabb270a
XSonicRU <miva-2002@mail.ru>
2016-09-27 22:02:41 +0300

    Finally all is working fine. How alphabet filtering in development.

--- src/Browser.java
+++ src/Browser.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.*;
 
 public class Browser {
     private static String readit() throws IOException {
@@ -9,11 +10,12 @@ public class Browser {
         BufferedReader br = new BufferedReader(isr);
         return br.readLine();
     }
-
+	private static boolean isfirstpath = true;
     private static String currentpath = "/";
 
     public static void makeitrock() throws IOException {
         while (true) {
+			System.out.println(currentpath);
             String[] allfiles = readalldirectory(currentpath);
             for (int i = 0; i < allfiles.length; i++) {
                 System.out.println(allfiles[i]);
@@ -34,13 +36,26 @@ public class Browser {
                     System.out.println("Try again, there is no such path or file.");
                 }
             }
-            currentpath = currentpath + "/" + input;
+			if(isfirstpath){
+				currentpath = currentpath + input;
+				isfirstpath = false;
+			}else{
+				currentpath = currentpath + "/"+ input;
+			}
+
             if (new File(currentpath).isFile()) {
                 while (true) {
                     System.out.println("You want to read this file?");
                     String sinput = readit();
                     if (sinput.equalsIgnoreCase("yes")) {
-                        //TODO:FINISH THAT!
+                        File filetoread = new File(currentpath);
+						FileInputStream fis = new FileInputStream(filetoread);
+						for(int i = 0;i<fis.available();i++){
+							System.out.print((char)fis.read());
+							System.out.println();
+							System.out.println("Файл прочитан!");
+							System.exit(0);
+						}
                     } else if (sinput.equalsIgnoreCase("no")) {
                         System.out.println("then have a good day!");
                         System.exit(0);
